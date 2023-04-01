e900d053339ee53e76e6e3d378de3a68264cad28
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-04-01 18:27:05 -0400

    added ProductImpl

---
+++ src/main/java/com/comp301/a08shopping/ProductImpl.java
@@ -0,0 +1,13 @@
+package com.comp301.a08shopping;
+
+public class ProductImpl implements Product{
+  @Override
+  public String getName() {
+    return null;
+  }
+
+  @Override
+  public double getBasePrice() {
+    return 0;
+  }
+}
