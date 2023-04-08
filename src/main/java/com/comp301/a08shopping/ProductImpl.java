45c36e988d0f35383aec71e137b2d2138d46bbcd
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-04-08 14:00:03 -0400

    attempted adept level

--- src/main/java/com/comp301/a08shopping/ProductImpl.java
+++ src/main/java/com/comp301/a08shopping/ProductImpl.java
@@ -3,6 +3,8 @@ package com.comp301.a08shopping;
 public class ProductImpl implements Product{
     private String name;
     private double basePrice;
+    private int inventory = 0;
+    private double percentOff;
 
     public ProductImpl(String name, double basePrice) {
         if (basePrice <= 0.00) {
@@ -20,4 +22,33 @@ public class ProductImpl implements Product{
   public double getBasePrice() {
     return basePrice;
   }
+public void setInventory(int amount) {
+    inventory += amount;
 }
+
+public int getInventory() {
+        return inventory;
+}
+
+public void setPercentOff(double percentOff) {
+        this.percentOff = percentOff;
+}
+
+public double getDiscountedPrice() {
+    double price = basePrice - (percentOff * basePrice);
+    double roundedPrice = Math.round(price * 100.0) / 100.0;
+    return roundedPrice;
+}
+
+public boolean hasDiscount() {
+        return percentOff > 0.00;
+}
+
+public boolean getInStock() {
+        return inventory > 0;
+}
+
+
+}
+
+
--- src/main/java/com/comp301/a08shopping/StoreImpl.java
+++ src/main/java/com/comp301/a08shopping/StoreImpl.java
@@ -1,53 +1,132 @@
 package com.comp301.a08shopping;
 
-import java.util.List;
+import com.comp301.a08shopping.events.*;import com.comp301.a08shopping.exceptions.OutOfStockException;import com.comp301.a08shopping.exceptions.ProductNotFoundException;import java.util.ArrayList;import java.util.List;
 public class StoreImpl implements Store{
     private String name;
+    private List<StoreObserver> observers = new ArrayList<>();
+    private List<Product> products = new ArrayList<>();
     public StoreImpl(String name) {
         this.name = name;
 
     }
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 
     @Override public void addObserver(StoreObserver observer) {
-
+        observers.add(observer);
     }
 
     @Override public void removeObserver(StoreObserver observer) {
-
+        observers.remove(observer);
     }
+
     @Override public List<Product> getProducts() {
-    return null;
+        return (List<Product>) ((ArrayList<Product>) products).clone();
     }
     @Override public Product createProduct(String name, double basePrice, int inventory) {
-    return null;
+        if (inventory < 0) {
+            throw new IllegalArgumentException("inventory must be positive");
+        }
+        ProductImpl newProd = new ProductImpl(name, basePrice);
+        newProd.setInventory(inventory);
+        return newProd;
     }
     @Override public ReceiptItem purchaseProduct(Product product) {
-    return null;
+        if (!products.contains(product)) {
+            throw new ProductNotFoundException();
+        }
+        if (product == null) {
+            throw new IllegalArgumentException();
+        }
+        if (((ProductImpl)product).getInventory() == 0) {
+            throw new OutOfStockException();
+        }
+        ReceiptItemImpl receipt = new ReceiptItemImpl(product.getName(), ((ProductImpl)product).getDiscountedPrice(), getName());
+        ((ProductImpl) product).setInventory(-1);
+
+        PurchaseEvent purchase = new PurchaseEvent(product, this);
+        notify(purchase);
+
+        return receipt;
     }
     @Override public void restockProduct(Product product, int numItems) {
-
+        if (!products.contains(product)) {
+            throw new ProductNotFoundException();
+        }
+        if (product == null || numItems < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (!getIsInStock(product)) {
+            BackInStockEvent back = new BackInStockEvent(product, this);
+            notify(back);
+        }
+        ((ProductImpl)product).setInventory(numItems);
     }
     @Override public void startSale(Product product, double percentOff) {
-
+        if (!products.contains(product)) {
+            throw new ProductNotFoundException();
+        }
+        if (product == null || !(percentOff > 0.0 && percentOff < 1.0)) {
+            throw new IllegalArgumentException();
+        }
+        ((ProductImpl)product).setPercentOff(percentOff);
+        SaleStartEvent newSale = new SaleStartEvent(product, this);
+        notify(newSale);
     }
     @Override public void endSale(Product product) {
-
+        if (!products.contains(product)) {
+            throw new ProductNotFoundException();
+        }
+        if (product == null) {
+            throw new IllegalArgumentException();
+        }
+        ((ProductImpl)product).setPercentOff(0);
+        SaleEndEvent endEvent = new SaleEndEvent(product, this);
+        notify(endEvent);
     }
     @Override public int getProductInventory(Product product) {
-    return 0;
+        if (!products.contains(product)) {
+            throw new ProductNotFoundException();
+        }
+        if (product == null) {
+            throw new IllegalArgumentException();
+        }
+        return ((ProductImpl)product).getInventory();
     }
     @Override public boolean getIsInStock(Product product) {
-    return false;
+        if (!products.contains(product)) {
+            throw new ProductNotFoundException();
+        }
+        if (product == null) {
+            throw new IllegalArgumentException();
+        }
+        return ((ProductImpl)product).getInStock();
     }
     @Override public double getSalePrice(Product product) {
-    return 0;
+        if (!products.contains(product)) {
+            throw new ProductNotFoundException();
+        }
+        if (product == null) {
+            throw new IllegalArgumentException();
+        }
+        return ((ProductImpl)product).getDiscountedPrice();
     }
     @Override public boolean getIsOnSale(Product product) {
-    return false;
+        if (!products.contains(product)) {
+            throw new ProductNotFoundException();
+        }
+        if (product == null) {
+            throw new IllegalArgumentException();
+        }
+        return ((ProductImpl)product).hasDiscount();
+    }
+
+    public void notify(StoreEventImpl storeEvent) {
+        for (StoreObserver looker : observers) {
+            looker.update(storeEvent);
+        }
     }
 
 
